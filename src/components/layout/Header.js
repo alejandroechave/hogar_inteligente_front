6ebef266cf5a2abd50a0bd0978b5d8a3b3785aec
unknown <psiomaragauna@gmail.com>
2024-11-27 20:54:57 -0300

    front

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,8 +7,6 @@ const Header = (props) => {
             <div className="holder">
                 <img src="images/logo.png" width="100" alt="Hogar Inteligente" />
                 <h1>Hogar Inteligente</h1>
-                <a href="`${process.env.REACT_APP_API_URL}/admin/login`" className="ingresar-btn">
-                <i class="bi bi-box-arrow-in-right"></i> Ingresar </a>
             </div>
         </header>
     );
